844be038bda3795329eaba7a065736ed9fa68b6b
shree7890 <shreekantadash831@gmail.com>
2021-10-04 22:02:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Blog from './components/Blog/Blog';
+import Contact from './components/Contact/Contact';
+import Notfound from './components/Notfound/Notfound';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import FooterBottom from './components/FooterBottom/FooterBottom';
+import Services from './components/Services/Services';
+import GetService from './components/GetService/GetService';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/services">
+            <Services></Services>
+          </Route>
+          <Route exact path="/teacher/:teacherId">
+            <GetService></GetService>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/blogs">
+            <Blog></Blog>
+          </Route>
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
+          <Route path="*">
+            <Notfound></Notfound>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+        <FooterBottom></FooterBottom>
+      </Router>
     </div>
   );
 }
