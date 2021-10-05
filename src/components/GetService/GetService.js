40299c1af3d182d47e30ad34f0379eb6dceba7bb
shree7890 <shreekantadash831@gmail.com>
2021-10-05 10:47:26 +0600

    final have project

--- src/components/GetService/GetService.js
+++ src/components/GetService/GetService.js
@@ -3,7 +3,6 @@ import { useParams } from 'react-router';
 import './GetService.css';
 const GetService = () => {
     const { teacherId } = useParams();
-
     return (
         <div className="py-5 text-center">
             <h2>{teacherId}</h2>
